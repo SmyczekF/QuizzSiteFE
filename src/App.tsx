79ec60b3fc85ab9a8a829be745491c8ddcb42b79
Filip Smyczek <smyczekf@interia.pl>
2023-12-21 17:01:53 +0100

    Added logos, color palette, app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { AppShell, Burger, Button, Group, Image, Skeleton, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import Main from './Main';
 
 function CollapseDesktop() {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
@@ -7,29 +8,33 @@ function CollapseDesktop() {
 
   return (
     <AppShell
-      header={{ height: 60 }}
+      header={{ height: 100 }}
       navbar={{
         width: 300,
         breakpoint: 'sm',
         collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
       }}
       padding="md"
+      bg={'#fefbed'}
     >
-      <AppShell.Header>
+      <AppShell.Header bg={'var(--background)'} style={{borderBottom: '1px solid var(--primary)'}}>
         <Group h="100%" px="md">
           <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
           <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+          <img src="/logoFull.png" alt='Logo Quizz World' height={'100%'}/>
         </Group>
       </AppShell.Header>
-      <AppShell.Navbar p="md">
-        Navbar
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
+      <AppShell.Navbar p="md" bg={'var(--background)'} style={{borderRight: '1px solid var(--primary)'}}>
+        <Button color='var(--primary)' style={{fontFamily: 'Staatliches Regular', fontSize: '2rem', letterSpacing: '2px'}}>Navbar</Button>
       </AppShell.Navbar>
-      <AppShell.Main>Main</AppShell.Main>
+      <AppShell.Main>
+        <Main />
+      </AppShell.Main>
+      <AppShell.Footer bg={'var(--background)'} style={{borderTop: '1px solid var(--primary)'}}>
+        <Group h="100%" px="md">
+          <Text size="xl" style={{fontFamily: 'Staatliches Regular', fontSize: '2rem', letterSpacing: '2px'}}>Footer</Text>
+        </Group>
+      </AppShell.Footer>
     </AppShell>
   );
 }
