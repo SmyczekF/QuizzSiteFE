1e76188d74eed36282c8b1beab5398924d619275
Filip Smyczek <smyczekf@interia.pl>
2024-01-10 20:43:21 +0100

    background color adjust, added subsites

--- src/Main.tsx
+++ src/Main.tsx
@@ -12,6 +12,13 @@ const Main = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/email-confirmed" element={<EmailConfirmed />} />
                 <Route path="/quizz/popular" element={<QuizzList title={'Popular Quizzes'} />} />
+                <Route path="/quizz/tv-shows" element={<QuizzList title={'TV Shows'} />} />
+                <Route path="/quizz/music" element={<QuizzList title={'Music'} />} />
+                <Route path="/quizz/movies" element={<QuizzList title={'Movies'} />} />
+                <Route path="/quizz/games" element={<QuizzList title={'Games'} />} />
+                <Route path="/quizz/trivia" element={<QuizzList title={'Trivia'} />} />
+                <Route path="/quizz/books" element={<QuizzList title={'Books'} />} />
+                <Route path="/quizz/other" element={<QuizzList title={'Other'} />} />
             </Routes>
         </Router>
         
