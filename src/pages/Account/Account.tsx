1766d5dacec30552a7717a3cec14698c3c3f7b01
Filip Smyczek <smyczekf@interia.pl>
2024-02-18 17:05:04 +0100

    account page started

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,11 +1,23 @@
+import { useContext } from 'react';
 import styles from './Account.module.scss';
+import { CredentialsContext } from '../../shared/providers/credentialsProvider';
+import { returnImage } from '../../shared/images/ImageReader';
 
 const Account = () => {
+
+    const credentialsContext = useContext(CredentialsContext);
+    
+    const userAvatar = returnImage(credentialsContext.user?.image);
+
     return (
         <div className={styles.view}>
             <div className={styles.header}>
-                
-                <h2 className={styles.title}>Account</h2>
+                {
+                    userAvatar
+                    ? <img src={userAvatar} alt="Author" className={styles.userIcon} style={{padding: 0, borderRadius: '50%'}}/>
+                    : <i className={`pi pi-user ${styles.userIcon}`}></i>
+                }
+                <h2 className={styles.title}>Welcome, {credentialsContext.user?.username}</h2>
             </div>
         </div>
     );
